f6fc8ee1fc3f7887498e4e017be9c9f3fa6813e6
Ole Sandum <ole@sandum.net>
2018-06-13 18:32:57 +0200

    add custom run.conf for test configuration

--- mk-server.sh
+++ mk-server.sh
@@ -7,6 +7,18 @@ rm -rvf ./build/image
 unzip -d ./build/image -u ./build/download/jboss-4.2.3.GA.zip
 
 # generate alpine based image:
+cat > ./build/image/run-test.conf <<EOD
+##
+## Bootstrap config, sourced from bin/run.sh
+## (a modified copy of bin/run.conf)
+##
+
+JAVA_OPTS="-Xms128m -Xmx512m"
+JAVA_OPTS="\$JAVA_OPTS -Djava.rmi.server.hostname=\$RMI_SERVER_HOSTNAME"
+JAVA_OPTS="\$JAVA_OPTS -Djboss.partition.name=\$JBOSS_PARTITION_NAME"
+EOD
+
+# generate alpine based image:
 cat > ./build/image/Dockerfile <<EOD
 FROM alpine:3.7
 RUN apk add --update ca-certificates && update-ca-certificates
@@ -17,6 +29,8 @@ RUN addgroup -S jboss && adduser -S -G jboss jboss -h /var/local/jboss
 
 # copy distribution folder and add symlinks to writable subfolders in server/all/{log,work,data,tmp}:
 COPY jboss-4.2.3.GA/ /usr/share/jboss/
+
+COPY run-test.conf /etc/jboss/
 
 RUN mkdir -pv /var/log/jboss /var/cache/jboss /var/local/jboss/work /var/local/jboss/data /tmp/jboss /etc/jboss
 RUN chown jboss:jboss /var/log/jboss /var/cache/jboss /var/local/jboss/work /var/local/jboss/data /tmp/jboss /etc/jboss
@@ -29,12 +43,13 @@ RUN ln -s /tmp/jboss /usr/share/jboss/server/all/tmp
 
 ENV LAUNCH_JBOSS_IN_BACKGROUND=1
 ENV TZ=Europe/Copenhagen
-ENV JAVA_OPTS=-Djava.rmi.server.hostname=localhost
-ENV BIND_IP=0.0.0.0
+ENV RMI_SERVER_HOSTNAME=localhost
+ENV JBOSS_PARTITION_NAME=TestPartition
+ENV RUN_CONF=/etc/jboss/run-test.conf
 
 USER jboss
 WORKDIR /usr/share/jboss
-CMD bin/run.sh -c all -b \$BIND_IP
+CMD bin/run.sh -c all -b 0.0.0.0
 
 EXPOSE 1098-1102 8080-8099 8443
 EOD
