60229c7ad868fec1e5a8fc3d8ed58796b0e35ae4
Ole Sandum <ole@sandum.net>
2018-10-03 11:36:51 +0200

    add simple pub/sub test on a known JMS topic

--- src/main/java/dk/br/JBossClientTest.java
+++ src/main/java/dk/br/JBossClientTest.java
@@ -21,9 +21,9 @@ import org.slf4j.LoggerFactory;
  */
 public class JBossClientTest {
     static Logger LOG = LoggerFactory.getLogger(JBossClientTest.class);
-    
+
     private final InitialContext ctx;
-    
+
     private JBossClientTest(InitialContext ctx) {
         this.ctx = ctx;
     }
@@ -32,29 +32,29 @@ public class JBossClientTest {
         Properties env = new Properties();
         env.setProperty("java.naming.factory.initial", "org.jnp.interfaces.NamingContextFactory");
         env.setProperty("jnp.disableDiscovery", "true");
-      //env.setProperty("java.naming.provider.url", "talr:1099");
+        env.setProperty("java.naming.provider.url", System.getProperty("java.naming.provider.url", "localhost:1099"));
 
         InitialContext ctx = new InitialContext(env);
-        
+
         if (args.length == 1 && "listen".equals(args[0])) {
             new JBossClientTest(ctx).listen();
         }
         else {
-            new JBossClientTest(ctx).speak();            
+            new JBossClientTest(ctx).speak();
         }
     }
-    
+
     public TopicConnection connect() throws NamingException, JMSException {
         Object o = ctx.lookup("ConnectionFactory");
         LOG.info("connectionFactory: {} ({})", o, o.getClass());
         TopicConnectionFactory tcf = (TopicConnectionFactory) o;
-        
+
         TopicConnection conn = tcf.createTopicConnection();
         LOG.info("connected {} ({})", conn, conn.getClass());
-        
+
         return conn;
     }
-    
+
     public void disconnect(TopicConnection conn) throws JMSException {
         if (conn != null) {
             conn.setExceptionListener(null);
@@ -62,15 +62,15 @@ public class JBossClientTest {
             LOG.info("disconnected");
         }
     }
-    
+
     public Topic getTestTopic() throws NamingException {
-        Object o = ctx.lookup("topic/testTopic");
-        LOG.info("topic: {} ({})", o, o.getClass());        
+        Object o = ctx.lookup("topic/allPointsBulletin");
+        LOG.info("topic: {} ({})", o, o.getClass());
         Topic testTopic = (Topic)o;
-        
+
         return testTopic;
     }
-    
+
     public void speak() throws NamingException, JMSException {
         TopicConnection conn = connect();
         try {
@@ -79,20 +79,20 @@ public class JBossClientTest {
 
             Topic testTopic = getTestTopic();
             TopicPublisher publisher = session.createPublisher(testTopic);
-            LOG.info("publisher: {} ({})", publisher, publisher.getClass());                       
+            LOG.info("publisher: {} ({})", publisher, publisher.getClass());
 
             MapMessage message = session.createMapMessage();
             message.setString("foo", "Hello, world!");
             message.setInt("bar", 42);
 
             publisher.publish(message);
-            LOG.info("published message!");        
+            LOG.info("published message!");
         }
         finally {
             disconnect(conn);
         }
     }
-        
+
     public void listen() throws NamingException, JMSException {
         TopicConnection conn = connect();
         TopicSession session = conn.createTopicSession(false, TopicSession.AUTO_ACKNOWLEDGE);
